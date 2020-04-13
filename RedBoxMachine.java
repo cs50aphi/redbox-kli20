4da77d65b5bb564f4c16e295e34cda82fd8f645a
Kate Li <kli20@punahou.edu>
2020-04-13 08:32:49 +0000

    finished DVD

--- RedBoxMachine.java
+++ RedBoxMachine.java
@@ -14,20 +14,21 @@ public class RedBoxMachine
    //Create an instance variable to hold all of the DVDs.
 
    /** the list of DVDs */
-   
+   ArrayList<DVD> movies = new ArrayList<String>();
+
    /** Constructs a Redbox Machine and fills it with DVDs
     *  Reads the file MovieList.txt so make sure that the
     *  file is in the same folder as the RedboxMachine.class
     *  file.
     */
-    
+
    public RedBoxMachine()
    {
       // Complete the constructor.
       // Leave this method. It will fill the machine with DVDs.
       fillMachine();
    }
-   
+
    /** Searches for the movie with the provided title and returns
     *  the position of the DVD in the list if the DVD is found and
     *  -1 if the DVD is not in the list.
@@ -38,10 +39,19 @@ public class RedBoxMachine
    public int searchForMovie(String title)
    {
       // Complete the method to search for a movie.
-      // If placement is -1, then the movie isn't there.
-      // Find the index of i if the movie is there.
-   }
-   
+      if (title.equals())
+      for (int i = 0; i < movies.size(); i++) {
+         if (movies.get(i).equals(title)) {
+            // Find the index of i if the movie is there.
+            return movies.indexOf(title);
+         }
+      }
+      else {
+         // If placement is -1, then the movie isn't there.
+         return -1;
+      }
+    }
+
    /** Returns the titles of all available DVD's in
     *  the machine.
     *  @return an ArrayList of Strings containing the
@@ -50,10 +60,18 @@ public class RedBoxMachine
    public ArrayList<String> getAvailableMovies()
    {
       // Complete the method to get all available movie titles.
-
+      ArrayList<String> availableTitles = new ArrayList<String>();
+      // Movie is avail if numcopies>=1
+      for (int i = 0; i < movies.size(); i++) {
+         if (movies[i].getNumCopies() >= 1) {
+            // Find the index of i if the movie is there.
+            availableTitles.add(movies[i].getTitle());
+         }
+      }
+      return availableTitles;
    }
-   
-   /** Allows a customer to rent a movie. When the movie is rented, the number 
+
+   /** Allows a customer to rent a movie. When the movie is rented, the number
     *  of available copies is reduced by 1. If there are 0 copies of the movie left
     *  after the transaction, the movie is removed from the list.
     *  @param title the title of the movie being rented.
@@ -63,19 +81,22 @@ public class RedBoxMachine
    public boolean rent(String title)
    {
       // Complete the method to rent a movie.
+      // check if movie exist, decrement
+      movies.decrementCopies()
+      // if copies is zero, remove from the list now
    }
-   
-   /** Allows a customer to return a movie. When the movie is returned, the number 
-    *  of available copies is increased by 1. If the movie was not already in the 
+
+   /** Allows a customer to return a movie. When the movie is returned, the number
+    *  of available copies is increased by 1. If the movie was not already in the
     *  machine, then the DVD is added to the list.
     *  @param title the title of the movie being returned.
     *  @return the DVD that was returned by the customer.
-    */ 
+    */
    public DVD returnMovie(String title)
    {
       // Complete the method to return a movie.
    }
-   
+
    /** This method fills the machine with movies. You do not have
     *  to do anything to the code in this method.
     */
@@ -85,7 +106,7 @@ public class RedBoxMachine
          Scanner sn = new Scanner(new File("MovieList.txt"));
          while(sn.hasNextLine())
             returnMovie(sn.nextLine());
-              
+
       }catch(FileNotFoundException e){
          String s = "File not found! Make sure that MovieList.txt ";
          s = s + "is in the same folder as the class.";
